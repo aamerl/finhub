1d9dbda6e6b25d975d79ad840799c6d6a8f21ac8
root <lhoussaine.aamer@outlook.fr>
2024-04-15 14:29:51 +0000

    add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,69 @@
+import unittest
+
+from utils import get_avro_schema
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_get_avro_schema(self):
+        expected_schema = """{
+  "type" : "record",
+  "name" : "message",
+  "namespace" : "FinnhubProducer",
+  "fields" : [ {
+    "name" : "data",
+    "type" : {
+      "type" : "array",
+      "items" : {
+        "type" : "record",
+        "name" : "data",
+        "fields" : [ {
+          "name" : "c",
+          "type":[
+            {
+               "type":"array",
+               "items":["null","string"],
+               "default":[]
+            },
+            "null"
+          ],
+          "doc" : "Trade conditions"
+        },
+        {
+          "name" : "p",
+          "type" : "double",
+          "doc" : "Price at which the stock was traded"
+        },
+        {
+          "name" : "s",
+          "type" : "string",
+          "doc" : "Symbol of a stock"
+        },
+        {
+          "name" : "t",
+          "type" : "long",
+          "doc" : "Timestamp at which the stock was traded"
+        },
+        {
+          "name" : "v",
+          "type" : "double",
+          "doc" : "Volume at which the stock was traded"
+        } ]
+      },
+      "doc" : "Trades messages"
+    },
+    "doc"  : "Contains data inside a message"
+  },
+  {
+    "name" : "type",
+    "type" : "string",
+    "doc"  : "Type of message"
+  } ],
+  "doc" : "A schema for upcoming Finnhub messages"
+}"""
+        actual_schema = get_avro_schema()
+        self.assertEqual(actual_schema, expected_schema)
+
+
+if __name__ == "__main__":
+    unittest.main()
